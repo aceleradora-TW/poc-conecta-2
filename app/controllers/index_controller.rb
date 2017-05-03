6c95bb73da75719957b90c6faf1bf0db74912c22
bernardofelipetto <be.felipetto@hotmail.com>
2017-05-03 11:36:59 -0300

    remv sahjd

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,3 +5,19 @@ end
 get '/search' do
   erb :cards
 end
+
+get '/az' do
+  erb :az
+end
+
+get '/za' do
+  erb :cards
+end
+
+get '/porte' do
+  erb :cards
+end
+
+get '/avaliacao' do
+  erb :cards
+end
